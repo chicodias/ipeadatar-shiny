1e5f5f0213f31e758cf9eb05caa7aaf924ba357d
chicodias <chicodias@outlook.com>
2023-09-24 20:34:14 -0300

    Adiciona outros sliders (falta server side)

--- shiny/app.R
+++ shiny/app.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(ipeadatar)
 library(ggplot2)
+library(dplyr)
 
 # Retrieve data
 datasets <- ipeadatar::available_series("br")
@@ -11,7 +12,12 @@ ui <- fluidPage(
   titlePanel("Explore Ipeadata Series"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("series", "Choose a Series:", datasets$code)
+      textOutput("nameDisplay"),
+      selectInput("series", "Choose a Series:", datasets$code),
+      selectInput("theme", "Theme", choices = unique(datasets$theme)),
+      selectInput("source", "Source", choices = unique(datasets$source), multiple = TRUE),
+      selectInput("freq", "Frequency", choices = unique(datasets$freq)),
+      radioButtons("status", "Status", choices = c("Active" = "active", "Inactive" = "inactive"))
     ),
     mainPanel(
       plotOutput("seriesPlot")
@@ -21,14 +27,35 @@ ui <- fluidPage(
 
 # Server logic
 server <- function(input, output) {
-  output$seriesPlot <- renderPlot({
-    selected_series_code <- input$series
-    selected_series <- ipeadatar::ipeadata(selected_series_code)
 
-    ggplot(selected_series, aes(x = date, y = value, color = tcode)) +
+## Temas <- unique(datasets$theme)
+  ## Source <- unique(datasets$source)
+  ## Frequencia <- unique(datasets$freq)
+
+  output$nameDisplay <- renderText({
+    # Example: Display series code as name
+    # You should replace this with actual logic to get the 'name' variable.
+    input$series
+  })
+
+  selected_series <- reactive({
+    series_data <- datasets %>%
+      filter(
+        code == input$series,
+        ## theme %in% input$theme,
+        ## source %in% input$source,
+        ## freq == input$freq,
+        ## status == input$status
+      )
+    if(nrow(series_data) > 0) ipeadatar::ipeadata(series_data$code) else NULL
+  })
+
+  output$seriesPlot <- renderPlot({
+    if(is.null(selected_series())) return(NULL)
+    ggplot(selected_series(), aes(x = date, y = value, color = tcode)) +
       geom_line() +
       labs(
-        title = paste("Series:", selected_series_code),
+        title = paste("Series:", input$series),
         x = "Data",
         y = "Value"
       )
